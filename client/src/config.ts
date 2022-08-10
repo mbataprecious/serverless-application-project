ecbcce601e62503a0d34ad3b0743d8519537b446
mbataprecious <mbataprecious9@gmail.com>
2022-08-10 08:07:21 +0100

    submission of project 4

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'atd68gupvc';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-odnfcukf.us.auth0.com',            // Auth0 domain
+  clientId: 'z8MlCT0yKbRwhsmVDlH1WuPnGgXNuZ5X',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+};
